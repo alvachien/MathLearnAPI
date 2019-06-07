3c9244cf382415d6087c67864092661ccce107be
Alva Chien <alvachien@163.com>
2019-06-08 00:10:59 +0800

    Add annoation to models

--- MathLearnAPI/Models/Knowledge.cs
+++ MathLearnAPI/Models/Knowledge.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace MathLearnAPI.Models
 {
     public partial class Knowledge
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
         public string Name { get; set; }
+        [Required]
         public string Content { get; set; }
     }
 }
